dcd570686273e5c94cfcc5eab4a428a04e0b1bce
Oscar A <oscar.g.alvarez00@gmail.com>
2024-11-20 22:26:37 -0500

    fixed fill_remaining()

--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,8 @@
+import sudoku_generator
+
 if __name__ == "__main__":
-    pass
+    print("yo")
+    b = sudoku_generator.generate_sudoku(9, 10)
+    #temp board print
+    for row in b:
+        print(row)
